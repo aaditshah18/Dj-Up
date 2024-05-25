b8b1acf4016ad90d06410075ab6b7bad36949d30
Aadit Shah <shah.aadit1@northeastern.edu>
2024-05-25 13:41:29 -0400

    Added Q object to filter offer on name

--- djup/offers/views.py
+++ djup/offers/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework import generics
 from offers.models import Offers
 from offers.serializers import OffersListSerializer
@@ -8,3 +9,13 @@ class OfferListAPIView(generics.ListCreateAPIView):
     queryset = Offers.objects.all()
     serializer_class = OffersListSerializer
     permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        name = self.request.query_params.get('name')
+        q_filters = Q()  # Start with an empty Q object
+        if name:
+            q_filters &= Q(name__icontains=name)  # Case-insensitive name search
+            
+        return queryset.filter(q_filters)
